2a505aff4365ef83e02ac1ba90eaa7566fa44216
Nayana-Nagaraj <nayana@devtools.in>
2022-03-30 12:18:27 +0530

    Commit from the VS Code

--- src/main/java/com/javatestdemo/MySum.java
+++ src/main/java/com/javatestdemo/MySum.java
@@ -14,7 +14,7 @@ public class MySum {
 		return a + b;
 	}
 	
-	publi static void main(String[] args) {
+	public static void main(String[] args) {
 		int numbers = Integer.parseInt(args[0]);
 		System.out.println(sumofnumbers(numbers));
 		
